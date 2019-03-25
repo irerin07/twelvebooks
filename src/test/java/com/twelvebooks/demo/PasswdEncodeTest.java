cee598f79cf016f2e5355b91f7138d46eaaa641c
irerin07 <counterchord89@gmail.com>
2019-03-25 17:02:56 +0900

    #20190325 비밀번호 암호화 테스트 코드

--- src/test/java/com/twelvebooks/demo/PasswdEncodeTest.java
+++ src/test/java/com/twelvebooks/demo/PasswdEncodeTest.java
@@ -1,4 +1,14 @@
 package com.twelvebooks.demo;
 
+import org.junit.Test;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
 public class PasswdEncodeTest {
+    @Test
+    public void encode()throws Exception{
+        PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        String encode = passwordEncoder.encode("359751");
+        System.out.println(encode);
+    }
 }
