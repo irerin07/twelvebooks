4ee0a3d16ed21e47e044c789e48f146911b1843c
coneseo <woon4754@gmail.com>
2019-03-27 18:06:28 +0900

    #20190327 Repository 수정

--- src/main/java/com/twelvebooks/demo/repository/ChallengeRepository.java
+++ src/main/java/com/twelvebooks/demo/repository/ChallengeRepository.java
@@ -2,6 +2,14 @@ package com.twelvebooks.demo.repository;
 
 import com.twelvebooks.demo.domain.Challenge;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ChallengeRepository extends JpaRepository<Challenge, Long> {
+    //1.User에 해당하는 List<Challenge>가져오는 쿼리
+    @Query(value = "SELECT c FROM Challenge c INNER JOIN FETCH c.user WHERE c.user.id = :id ORDER BY c.id DESC",
+            countQuery = "SELECT count(c) FROM Challenge c")
+    public List<Challenge> getChallenges(@Param("id") Long id);
 }
--- src/main/java/com/twelvebooks/demo/repository/DiaryRepository.java
+++ src/main/java/com/twelvebooks/demo/repository/DiaryRepository.java
@@ -2,6 +2,17 @@ package com.twelvebooks.demo.repository;
 
 import com.twelvebooks.demo.domain.Diary;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface DiaryRepository extends JpaRepository<Diary, Long> {
+    //1.Challenge에 해당하는 다이어리 가져오기
+    @Query(value = "SELECT d FROM Diary d INNER JOIN FETCH d.challenge WHERE d.challenge.id = :id")
+    public List<Diary> getDiariesByChallengeId(@Param("id") long id);
+
+    //2.User에 해당하는 다이어리 가져오기
+    @Query(value = "SELECT d FROM Diary d INNER JOIN FETCH d.user WHERE d.user.id = :id")
+    public List<Diary> getDiariesByUserId(@Param("id") long id);
 }
