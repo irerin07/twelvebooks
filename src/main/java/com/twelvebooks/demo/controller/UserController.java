30fc5b5f698c2c1b173e9fa5bf69f1408804b080
irerin07 <counterchord89@gmail.com>
2019-03-27 18:09:24 +0900

    #20190327 인덱스 페이지 수정 및 UserController 추가

--- src/main/java/com/twelvebooks/demo/controller/UserController.java
+++ src/main/java/com/twelvebooks/demo/controller/UserController.java
@@ -1,4 +1,42 @@
 package com.twelvebooks.demo.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/users")
+@RequiredArgsConstructor
 public class UserController {
+
+
+    @GetMapping("/login")
+    public String login(
+            @RequestParam(name = "fail",
+                    required = false,
+                    defaultValue = "false") String errorFlag){
+        return "users/login";
+    }
+
+    @GetMapping("/join")
+    public String joinForm(){
+        return "users/join";
+    }
+
+    @GetMapping("/welcome")
+    public String welcome(){
+        return "users/index";
+    }
+
+    @GetMapping("/challenge")
+    public String challenge(){
+        return "users/challenge";
+    }
+
+    @GetMapping("/delete")
+    public String delete(){
+        return "users/delete";
+    }
 }
