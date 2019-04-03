705e41634fc1cd4965800f78184f60cb020d2112
coneseo <woon4754@gmail.com>
2019-04-03 15:10:05 +0900

    #2019.04.03 도전 등록 폼 수정, 날짜 자동 계산 function() 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -8,6 +8,7 @@ insert into users(id, email ,name, comment,  passwd, regdate) values (7, 'counte
 insert into users(id, email ,name, comment,  passwd, regdate) values (8, 'counterchord19@gmail.com', '송경수', '만나서 반가워요', '{bcrypt}$2a$10$cSJx.TaMP5iKxX2a5omtrulHsNPSsVhtdx.lzS/RO2i24dQuKlOTC', now());
 insert into users(id, email ,name, comment,  passwd, regdate) values (9, 'counterchord09@gmail.com', '조경수', '만나서 반가워요', '{bcrypt}$2a$10$cSJx.TaMP5iKxX2a5omtrulHsNPSsVhtdx.lzS/RO2i24dQuKlOTC', now());
 insert into users(id, email ,name, comment,  passwd, regdate) values (10, 'counterchord99@gmail.com', '임경수', '만나서 반가워요', '{bcrypt}$2a$10$cSJx.TaMP5iKxX2a5omtrulHsNPSsVhtdx.lzS/RO2i24dQuKlOTC', now());
+insert into users(id, email ,name, comment,  passwd, regdate) values (11, 'scw@gmail.com', '서청원', '열심히 읽을게요', '{bcrypt}$2a$10$uJx7glu3F.5YDxTyLZx8MObBJBf2Ww/T27K8Ckh3UVM.8nIriqnGW', now());
 
 insert into books(id, isbn ,title, author,  translator, thumbnail_image, publisher) values (1, '9788968481475', '이것이 자바다', '신용권','-', 'http://www.hanbit.co.kr/data/books/B1460673937_l.jpg', '한빛미디어');
 insert into books(id, isbn ,title, author,  translator, thumbnail_image, publisher) values (2, '9788968481476', '이것도 자바다', '신용만','-', 'http://www.hanbit.co.kr/data/books/B1460673937_l.jpg', '두빛미디어');
@@ -29,6 +30,7 @@ insert into user_roles(user_id, role_id) values (7, 2);
 insert into user_roles(user_id, role_id) values (8, 2);
 insert into user_roles(user_id, role_id) values (9, 2);
 insert into user_roles(user_id, role_id) values (10, 2);
+insert into user_roles(user_id, role_id) values (11, 2);
 
 insert into challenges(id, start_date, end_date, days, visibility, status, user_id, book_id) values(1, '2019-03-25 23:59:59', '2019-04-23 23:59:59',30, 1,  '읽는중', 1, 1);
 insert into challenges(id, start_date, end_date, days, visibility, status, user_id, book_id) values(2, '2019-03-26 23:59:59', '2019-04-24 23:59:59',30, 1,  '읽는중', 2, 2);
