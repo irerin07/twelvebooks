6d2ac98ae089010a849dcb98fa67b3533ad756b6
irerin07 <counterchord89@gmail.com>
2019-04-23 20:40:15 +0900

    #20190423 library add to diary

--- src/main/java/com/twelvebooks/twelvebook/service/implementation/DiaryServiceImpl.java
+++ src/main/java/com/twelvebooks/twelvebook/service/implementation/DiaryServiceImpl.java
@@ -1,7 +1,9 @@
 package com.twelvebooks.twelvebook.service.implementation;
 
 import com.twelvebooks.twelvebook.domain.Diary;
+import com.twelvebooks.twelvebook.repository.DiaryRepository;
 import com.twelvebooks.twelvebook.service.DiaryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -9,9 +11,13 @@ import java.util.List;
 @Service
 public class DiaryServiceImpl implements DiaryService {
 
+    @Autowired
+    DiaryRepository diaryRepository;
+
     @Override
     public Diary addDiary(Diary diary) {
-        return null;
+        Diary save = diaryRepository.save(diary);
+        return save;
     }
 
     @Override
