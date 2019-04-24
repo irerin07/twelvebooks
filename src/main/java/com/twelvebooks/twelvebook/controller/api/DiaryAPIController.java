b03dd4631f5f260c620aa0216deb123ba709e9e9
irerin07 <counterchord89@gmail.com>
2019-04-24 16:36:06 +0900

    #20190424 thymeleaf 수정

--- src/main/java/com/twelvebooks/twelvebook/controller/api/DiaryAPIController.java
+++ src/main/java/com/twelvebooks/twelvebook/controller/api/DiaryAPIController.java
@@ -3,6 +3,7 @@ package com.twelvebooks.twelvebook.controller.api;
 import com.twelvebooks.twelvebook.domain.Diary;
 import com.twelvebooks.twelvebook.dto.DiaryDto;
 import com.twelvebooks.twelvebook.repository.DiaryRepository;
+import com.twelvebooks.twelvebook.service.ChallengeService;
 import com.twelvebooks.twelvebook.service.DiaryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -15,12 +16,19 @@ public class DiaryAPIController {
 
     @Autowired
     DiaryService diaryService;
+    @Autowired
+    ChallengeService challengeService;
 
     @PostMapping
     public void postReview(@RequestBody DiaryDto diaryDto){
-        System.out.println("DiaryDto: " + diaryDto.getContent());
         Diary diary = new Diary();
         diary.setContent(diaryDto.getContent());
+        System.out.println("DiaryDto: " + diaryDto.getContent());
+        diary.setDay(diaryDto.getDay());
+        System.out.println("DiaryDto: " + diaryDto.getDay());
+        diary.setChallenge(challengeService.getChallengeDetail(diaryDto.getChalid()));
+        System.out.println("DiaryDto: " + diaryDto.getChalid());
+
         diaryService.addDiary(diary);
 
 
