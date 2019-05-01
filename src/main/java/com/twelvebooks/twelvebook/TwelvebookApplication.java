6190144369839b33964cfd3f0314a20e2d4b44e9
irerin07 <counterchord89@gmail.com>
2019-05-01 14:34:21 +0900

    #20190501 aop 추가 및 library 부분 수정

--- src/main/java/com/twelvebooks/twelvebook/TwelvebookApplication.java
+++ src/main/java/com/twelvebooks/twelvebook/TwelvebookApplication.java
@@ -2,8 +2,14 @@ package com.twelvebooks.twelvebook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+@EnableConfigurationProperties
+@EnableAspectJAutoProxy
+//@EnableAspectAutoProxy를 명시하면 Spring AOP를 사용하기 위한 첫 준비가 끝난다.
+//@EnableAspectAutoProxy은 XML 기반의 ApplicationContext 설정에서의 <aop:aspectj-autoproxy />와 동일한 기능을 한다.
 public class TwelvebookApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/twelvebooks/twelvebook/aop/RequestLoggingAspect.java
@@ -0,0 +1,57 @@
+package com.twelvebooks.twelvebook.aop;
+
+import com.google.common.base.Joiner;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Component // 1
+@Aspect // 2
+public class RequestLoggingAspect {
+    private static final Logger logger = LoggerFactory.getLogger(RequestLoggingAspect.class);
+
+    private String paramMapToString(Map<String, String[]> paramMap) {
+        return paramMap.entrySet().stream()
+                .map(entry -> String.format("%s -> (%s)",
+                        entry.getKey(), Joiner.on(",").join(entry.getValue())))
+                .collect(Collectors.joining(", "));
+    }
+
+    @Pointcut("within(com.twelvebooks.twelvebook.controller.api..*)") // 3
+    public void onRequest() {}
+
+    @After("execution(* com.twelvebooks.twelvebook.controller.api.*.*(..))") // 4
+    public Object doLogging(JoinPoint joinPoint) throws Throwable {
+        HttpServletRequest request = // 5
+                ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
+
+        Map<String, String[]> paramMap = request.getParameterMap();
+        String params = "";
+        if (paramMap.isEmpty() == false) {
+            params = " [" + paramMapToString(paramMap) + "]";
+        }
+
+        long start = System.currentTimeMillis();
+        try {
+            return joinPoint.getArgs();
+//            return pjp.proceed(pjp.getArgs()); // 6
+        } finally {
+            long end = System.currentTimeMillis();
+            logger.info("Request: {} {}{} < {} ({}ms)", request.getMethod(), request.getRequestURI(),
+                    params, request.getRemoteHost(), end - start);
+        }
+    }
+}
+
