86aa136925c49dd45f51e5d91a1bf058e27bf5ca
kim-daeyong <kdy2353@gmail.com>
2019-05-03 20:09:01 +0900

    북마크 체크삭제, 전체삭제, 개별삭제

--- src/main/java/com/twelvebooks/twelvebook/controller/api/BookmarkAPIController.java
+++ src/main/java/com/twelvebooks/twelvebook/controller/api/BookmarkAPIController.java
@@ -5,12 +5,15 @@ import com.twelvebooks.twelvebook.controller.BookmarkController;
 import com.twelvebooks.twelvebook.domain.Book;
 import com.twelvebooks.twelvebook.domain.Bookmark;
 import com.twelvebooks.twelvebook.domain.User;
+import com.twelvebooks.twelvebook.dto.BookmarkDto;
 import com.twelvebooks.twelvebook.repository.BookmarkRepository;
 import com.twelvebooks.twelvebook.repository.UserRepository;
 import com.twelvebooks.twelvebook.service.BookmarkService;
 import com.twelvebooks.twelvebook.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -39,62 +42,17 @@ public class BookmarkAPIController {
     @Autowired
     UserRepository userRepository;
 
-    @GetMapping("/delete/{id}")
-    public int bookmarkDelete(@PathVariable(name="id") Long id, Principal principal) {
-        int result = 0;
-        User user =  userService.getUserByEmail(principal.getName());
-        if(user != null) {
-            bookmarkRepository.deleteById(id);
-            result = 1;
-        }
-        return result;
-
-    }
-
-
-//    @DeleteMapping(value = "/{id}")
-//    public int delete(@PathVariable(value = "id") Long id, Principal principal){
-//        User user =  userService.getUserByEmail(principal.getName());
-//        bookmarkService.bookmarkDelete(id);
-//
-//        return bookmarkService.bookmarkList(user.getId()).size();
-//
-//    }
-
-    @PostMapping("/selectdel")
-    public int selectDelete(@RequestParam(value = "chbox[]") List<String> chArr, Bookmark bookmark, Principal principal){
-
-
+    @DeleteMapping(value = "/{bookmarkId}")
+    public int delete(@PathVariable(value = "bookmarkId") Long id, Principal principal){
         User user = userService.getUserByEmail(principal.getName());
+        bookmarkService.bookmarkDelete(id);
+        bookmarkService.bookmarkList(id);
 
-        int result = 0;
-        long bookmarkId = 0;
-        long userId = user.getId();
-
-
-        if(user != null) {
-            bookmark.setUser(user);
-
-            for(String i : chArr) {
-                bookmarkId = Integer.parseInt(i);
-                bookmark.setId(bookmarkId);
-                bookmarkService.selectDelete(bookmark, userId);
-            }
-            result = 1;
-        }
-        return result;
-
-//
-//        return bookmarkService.bookmarkList(user.getId()).size();
+        return bookmarkService.bookmarkList(user.getId()).size();
     }
 
 
-//    @DeleteMapping(value = "/{listId}")
-//    public int delete(@PathVariable(value = "listId") Long id, Principal principal){
-//        User user = userService.getUserByEmail(principal.getName());
-//        bookmarkService.bookmarkDelete(id);
-//        return bookmarkService.selectAllByUserId(user.getId()).size();
-//    }
+
 
 
     @PostMapping("/add")
