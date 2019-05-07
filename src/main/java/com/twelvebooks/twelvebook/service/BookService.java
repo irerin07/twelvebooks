6702520711c550bfd46ac9be0f09f0b3327ec854
coneseo <woon4754@gmail.com>
2019-05-07 18:48:39 +0900

    #20190507 도전 등록시 책의 유무에 따라 책을 등록하거나 불러오는 기능 구현

--- src/main/java/com/twelvebooks/twelvebook/service/BookService.java
+++ src/main/java/com/twelvebooks/twelvebook/service/BookService.java
@@ -5,6 +5,9 @@ import com.twelvebooks.twelvebook.domain.Book;
 import java.util.List;
 
 public interface BookService {
-
+    //isbn에 해당하는 책 찾기
     Book getBookByIsbn(String isbn);
+
+    //책 등록하기
+    Book addBook(Book book);
 }
--- src/main/java/com/twelvebooks/twelvebook/service/implementation/BookServiceImpl.java
+++ src/main/java/com/twelvebooks/twelvebook/service/implementation/BookServiceImpl.java
@@ -1,16 +1,31 @@
 package com.twelvebooks.twelvebook.service.implementation;
 
 import com.twelvebooks.twelvebook.domain.Book;
+import com.twelvebooks.twelvebook.repository.BookRepository;
 import com.twelvebooks.twelvebook.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class BookServiceImpl implements BookService {
+    @Autowired
+    BookRepository bookRepository;
 
     @Override
     public Book getBookByIsbn(String isbn) {
-        return null;
+
+        Book book = bookRepository.getBookByIsbn(isbn);
+
+        return book;
+    }
+
+    @Override
+    public Book addBook(Book book) {
+
+        Book result =  bookRepository.save(book);
+
+        return result;
     }
 }
