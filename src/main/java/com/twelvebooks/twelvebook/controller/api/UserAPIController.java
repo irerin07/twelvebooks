0ae7d2891474d5992ec7370f0290c4ec63664531
coneseo <woon4754@gmail.com>
2019-05-19 23:10:45 +0900

    #20190519 admin 수정

--- src/main/java/com/twelvebooks/twelvebook/controller/api/UserAPIController.java
+++ src/main/java/com/twelvebooks/twelvebook/controller/api/UserAPIController.java
@@ -1,15 +1,22 @@
 package com.twelvebooks.twelvebook.controller.api;
 
+import com.twelvebooks.twelvebook.domain.User;
 import com.twelvebooks.twelvebook.dto.UserAPIResultDto;
+import com.twelvebooks.twelvebook.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/api/users")
 @RestController
 @RequiredArgsConstructor
 public class UserAPIController {
-
+    @Autowired
+    UserService userService;
 
     @GetMapping
     public ResponseEntity<UserAPIResultDto> getUserById(@RequestParam Long userId){
@@ -17,7 +24,10 @@ public class UserAPIController {
     }
 
     @PostMapping
-    public ResponseEntity<UserAPIResultDto> modifyUserRole(){
+    public ResponseEntity<UserAPIResultDto> modifyUserRole(@RequestParam String roleName){
+        Authentication loginUser = SecurityContextHolder.getContext().getAuthentication();
+        User user = userService.getUserByEmail(loginUser.getName());
+
         return null;
     }
 
